6fbae6501657022ab33b1a6ededafbbd386dbf39
Ray Chow <raychow.info@gmail.com>
2018-05-20 20:23:01 +0800

    first commit

---
+++ lxr_analyse.py
@@ -0,0 +1,97 @@
+import datetime
+import json
+import numpy
+import os
+import requests
+import scipy.stats
+import time
+
+import lxr_fetch
+
+
+output_folder = './output'
+output_file_template = output_folder + '/{}-{}.csv'
+aggregate_file_template = output_folder + '/aggregated-' + time.strftime('%Y%m%d', time.localtime()) + '.csv'
+
+def get_by_path(value, path):
+    keys = path.split('.')
+    result = value
+    for key in keys:
+        if not result:
+            return result
+        result = result.get(key)
+    return result
+
+def get_market_value(metric):
+    return metric['market_value']
+
+def get_pe(metric):
+    return get_by_path(metric, 'pe_ttm.weightedAvg')
+
+def get_pb(metric):
+    return get_by_path(metric, 'pb.weightedAvg')
+
+def get_ps(metric):
+    return get_by_path(metric, 'ps_ttm.weightedAvg')
+
+def get_pes(metrics):
+    return [get_pe(m) for m in metrics]
+
+def get_pbs(metrics):
+    return [get_pb(m) for m in metrics]
+
+def get_pss(metrics):
+    return [get_ps(m) for m in metrics]
+
+def calc_temperature(sample):
+    return map(lambda i: scipy.stats.norm.cdf(
+        sample[i], numpy.mean(sample[:i + 1]), numpy.std(sample[:i + 1])) * 100, range(len(sample)))
+
+def format_json_date(json_date):
+    utc = datetime.datetime.strptime(metric['date'], '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=datetime.timezone.utc)
+    cst = utc.astimezone(tz=None)
+    return cst.strftime('%Y%m%d')
+
+
+if __name__ == '__main__':
+    aggregated = []
+
+    os.makedirs(output_folder, exist_ok=True)
+
+    for stock_code, stock_code_desc in lxr_fetch.stock_codes.items():
+        print('processing', stock_code, stock_code_desc)
+        stock_code_name = stock_code_desc["name"]
+
+        data_file_path = lxr_fetch.get_data_file_path(stock_code, stock_code_name)
+        try:
+            with open(data_file_path, 'r') as f:
+                metrics = json.load(f)
+        except FileNotFoundError:
+            print('data file', data_file_path, 'not exists')
+            continue
+
+        metrics = [m for m in metrics if 'pe_ttm' in m and 'pb' in m]
+        pes_temp = calc_temperature(get_pes(metrics))
+        pbs_temp = calc_temperature(get_pbs(metrics))
+        # pss_temp = calc_temperature(get_pss(metrics))
+
+        with open(output_file_template.format(stock_code, stock_code_name), 'w') as f:
+            f.write('date,market_value,pe,pb,pe_temp,pb_temp,(pe_temp+pb_temp)/2\n')
+            for metric, pe_temp, pb_temp in zip(metrics, pes_temp, pbs_temp):
+                f.write('{},{},{},{},{},{},{}\n'.format(
+                    format_json_date(metric['date']),
+                    get_market_value(metric), get_pe(metric), get_pb(metric),
+                    pe_temp, pb_temp, (pe_temp + pb_temp) / 2))
+                last_metric, last_pe_temp, last_pb_temp = metric, pe_temp, pb_temp
+        
+        aggregated.append([
+            format_json_date(last_metric['date']), stock_code, stock_code_name,
+            get_market_value(metric), get_pe(last_metric), get_pb(last_metric), get_ps(last_metric),
+            last_pe_temp, last_pb_temp, (last_pe_temp + last_pb_temp) / 2
+        ])
+
+    print('aggregate to file', aggregate_file_template)
+    with open(aggregate_file_template, 'w') as f:
+        f.write('date,stock_code,stock_code_name,market_value,pe,pb,ps,pe_temp,pb_temp,(pe_temp+pb_temp)/2\n')
+        for a in aggregated:
+            f.write(','.join(map(str, a)) + '\n')
---
+++ lxr_fetch.py
@@ -0,0 +1,110 @@
+import datetime
+import json
+import os
+import requests
+import shutil
+import time
+
+
+config_path = './config.json'
+
+metrics_url = 'https://www.lixinger.com/api/open/a/indice/fundamental-info'
+stock_codes = {
+    '000905': { 'name': '中证500', 'source': 'api' },
+    '000016': { 'name': '上证50', 'source': 'api' },
+    '000300': { 'name': '沪深300', 'source': 'api' },
+    '399673': { 'name': '创业板50', 'source': 'api' },
+    '399006': { 'name': '创业板指', 'source': 'api' },
+    '000015': { 'name': '红利指数', 'source': 'api' },
+    '399903': { 'name': '中证100', 'source': 'api' },
+    '000922': { 'name': '中证红利', 'source': 'api' },
+    '000925': { 'name': '基本面50', 'source': 'api' },
+    '399324': { 'name': '深证红利', 'source': 'api' },
+    '399550': { 'name': '央视50', 'source': 'api' },
+    '399005': { 'name': '中小板指', 'source': 'api' },
+    '399330': { 'name': '深证100', 'source': 'api' },
+    '000009': { 'name': '上证380', 'source': 'api' },
+    '10010000010001': { 'name': '恒生指数', 'source': 'http' },
+    '10010000010002': { 'name': '国企指数', 'source': 'http' },
+}
+
+# 有些指数 api 暂时拿不到, 先从 http 拿
+http_metrics_url = 'https://www.lixinger.com/api/analyt/stock-collection/price-metrics/load'
+
+expected_metrics = ['pe_ttm', 'pb', 'ps_ttm', 'dividend_r', 'close_point', 'market_value']
+
+data_folder = './data'
+data_file_template = data_folder + '/{}-{}.json'
+
+now = time.strftime('%Y%m%d%H%M%S', time.localtime())
+five_years_ago = (datetime.datetime.now() - datetime.timedelta(days=365*5)).strftime('%Y-%m-%d')
+
+
+def fetch_metrics(token, start_date, stock_code):
+    payload = {
+        'token': token,
+        'startDate': start_date,
+        'stockCodes': [stock_code],
+        'metrics': expected_metrics,
+    }
+    r = requests.post(metrics_url, json=payload)
+    return r.json()
+
+def fetch_http_metrics(http_cookies, stock_code):
+    payload = {
+        'stockIds': [stock_code],
+        'dateFlag': 'day',
+        'granularity': 'y_10',
+        'metricNames': ['pe_ttm', 'pb', 'ps_ttm', 'dividend_r', 'close_point', 'market_value'],
+        'metricTypes': ['weightedAvg'],
+    }
+    r = requests.post(http_metrics_url, json=payload, cookies=http_cookies)
+    return r.json()
+
+def get_data_file_path(stock_code, stock_code_name):
+    return data_file_template.format(stock_code, stock_code_name)
+
+def download_metric(config, stock_code, stock_code_name, source):
+    print('downloading', stock_code, stock_code_name)
+
+    if source == 'http':
+        incremental_metrics = fetch_http_metrics(config['http_cookies'], stock_code)
+    else:
+        incremental_metrics = fetch_metrics(config['token'], five_years_ago, stock_code)
+
+    print('download success')
+    if not incremental_metrics:
+        print('[error] no data return, maybe something wrong')
+        return
+
+    incremental_metrics.sort(key=lambda e: e['date'])
+    earliest_incremental_date = incremental_metrics[0]['date']
+
+    data_file_path = get_data_file_path(stock_code, stock_code_name)
+    backup_file_path = data_file_path + '.' + now
+
+    inventory_metrics = []
+    try:
+        shutil.copyfile(data_file_path, backup_file_path)
+        print(data_file_path, 'backuped to file', backup_file_path)
+        with open(data_file_path, 'r') as f:
+            inventory_metrics = json.load(f)
+        print('load inventory metrics from', data_file_path)
+    except FileNotFoundError:
+        print('data file', data_file_path, 'not exists')
+    
+    inventory_metrics = list(filter(lambda e: e['date'] < earliest_incremental_date, inventory_metrics))
+    inventory_metrics.extend(incremental_metrics)
+    with open(data_file_path, 'w') as f:
+        json.dump(inventory_metrics, f)
+    print('save data to', data_file_path)
+
+
+if __name__ == '__main__':
+    with open(config_path, 'r') as f:
+        config = json.load(f)
+
+    os.makedirs(data_folder, exist_ok=True)
+
+    for stock_code, stock_code_desc in stock_codes.items():
+        download_metric(config, stock_code, stock_code_desc['name'], stock_code_desc['source'])
