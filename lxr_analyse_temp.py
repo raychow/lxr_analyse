ae80fd311c93cd1129af3f2a740cffe5fb9ef46d
Ray Chow <raychow.info@gmail.com>
2018-05-22 02:41:43 +0800

    add percentile calculate

--- lxr_analyse_temp.py
+++ lxr_analyse_temp.py
@@ -46,8 +46,9 @@ def get_pbs(metrics):
 def get_pss(metrics):
     return [get_ps(m) for m in metrics]
 
-def calc_percent(sample):
-    return map(lambda s: scipy.stats.percentileofscore(sample, s), sample)
+def calc_temperature(sample):
+    return map(lambda i: scipy.stats.norm.cdf(
+        sample[i], numpy.mean(sample[:i + 1]), numpy.std(sample[:i + 1])) * 100, range(len(sample)))
 
 def format_json_date(json_date):
     utc = datetime.datetime.strptime(metric['date'], '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=datetime.timezone.utc)
@@ -73,28 +74,28 @@ if __name__ == '__main__':
             continue
 
         metrics = [m for m in metrics if 'pe_ttm' in m and 'pb' in m]
-        pes_percent = calc_percent(get_pes(metrics))
-        pbs_percent = calc_percent(get_pbs(metrics))
-        # pss_percent = calc_percent(get_pss(metrics))
+        pes_temp = calc_temperature(get_pes(metrics))
+        pbs_temp = calc_temperature(get_pbs(metrics))
+        # pss_temp = calc_temperature(get_pss(metrics))
 
         with open(output_file_template.format(stock_code, stock_code_name), 'w') as f:
-            f.write('date,close_point,market_value,pe,pb,pe_percent,pb_percent\n')
-            for metric, pe_percent, pb_percent in zip(metrics, pes_percent, pbs_percent):
-                f.write('{},{},{},{},{},{},{}\n'.format(
+            f.write('date,close_point,market_value,pe,pb,pe_temp,pb_temp,(pe_temp+pb_temp)/2\n')
+            for metric, pe_temp, pb_temp in zip(metrics, pes_temp, pbs_temp):
+                f.write('{},{},{},{},{},{},{},{}\n'.format(
                     format_json_date(metric['date']), get_close_point(metric),
                     get_market_value(metric), get_pe(metric), get_pb(metric),
-                    pe_percent, pb_percent))
-                last_metric, last_pe_percent, last_pb_percent = metric, pe_percent, pb_percent
+                    pe_temp, pb_temp, (pe_temp + pb_temp) / 2))
+                last_metric, last_pe_temp, last_pb_temp = metric, pe_temp, pb_temp
         
         aggregated.append([
             format_json_date(last_metric['date']), stock_code, stock_code_name,
             get_close_point(last_metric), get_market_value(last_metric),
             get_pe(last_metric), get_pb(last_metric), get_ps(last_metric),
-            last_pe_percent, last_pb_percent
+            last_pe_temp, last_pb_temp, (last_pe_temp + last_pb_temp) / 2,
         ])
 
     print('aggregate to file', aggregate_file_template)
     with open(aggregate_file_template, 'w') as f:
-        f.write('date,stock_code,stock_code_name,close_point,market_value,pe,pb,ps,pe_percent,pb_percent\n')
+        f.write('date,stock_code,stock_code_name,close_point,market_value,pe,pb,ps,pe_temp,pb_temp,(pe_temp+pb_temp)/2\n')
         for a in aggregated:
             f.write(','.join(map(str, a)) + '\n')
